f49d0312995dfd7b762592fac4300d73bcf09073
Kichul Kim <hi.jigoo@gmail.com>
2025-11-09 15:13:15 +0900

    packaging artifact flow

---
+++ python_app_packaging_artifacts/main.py
@@ -0,0 +1,8 @@
+def add(a: int, b: int) -> int:
+    """Return the sum of two numbers."""
+    return a + b
+
+
+def multiply(a: int, b: int) -> int:
+    """Return the product of two numbers."""
+    return a * b
